fc509563565b23c156c8d6f2fa126813c684979c
Peter Park <peterxpark@gmail.com>
2014-10-22 16:38:03 -0400

    convert file creates new DB file ready for use by site.

--- convert-rawfile.php
+++ convert-rawfile.php
@@ -1,4 +1,21 @@
 <?php
+/*
+Oct 2014
+Version 1 
+
+Raw file is from: http://www.forbes.com/sites/kevinkruse/2013/05/28/inspirational-quotes/
+
+This script will read this raw file and convert it into a data file for the site to use to randomly grab quotes from.
+
+For unknown reason, PHP cannot read/understand the "-" dash hypen character. I tried doing strpos($line, "-") or explode("-", $line) and it doesn't work.
+Hence the function findQuoteend() existence
+*/
+
+print "Program Starting...<br />";
+$ouFilename = "QuotesFromForbes.txt";
+$outfile = fopen($ouFilename, "w");
+
+
 $filename = "rawfile-QuotesFromForbes.txt";
 $fp = fopen($filename, "r");
     while (($buffer = fgets($fp, 4096)) !== false) {
@@ -7,16 +24,19 @@ $fp = fopen($filename, "r");
     		$line = substr($line, strpos($buffer, " ")+1);    //skips the quote # at beginning of line
 
             $end_point = findQuoteEnd($line);
-            $author = substr($line, $end_point+1);
-            $quote = substr($line, 0, $end_point);
+            $author = trim(substr($line, $end_point+1));
+            $quote = trim(substr($line, 0, $end_point));
 
-            print $quote . "by " . $author . "<br />";
+            //print $quote . "by " . $author . "<br />";
+            fwrite($outfile, $quote . ";" . $author . "\r\n");
         }
     }
     if (!feof($fp)) {
         echo "Error: unexpected fgets() fail\n";
     }
 fclose($fp);
+fclose($outfile);
+print 'Program Finished, go see your <a href="'. $ouFilename . '">new file</a>';
 
 
 function findQuoteEnd($line) {
@@ -25,7 +45,6 @@ function findQuoteEnd($line) {
         $letter = $line[$i];
         if (!preg_match('/[a-zA-Z]/', $letter) AND $letter != "." AND $letter != " ") 
             return $i;
-            //return substr($line, $i+1);            
     }
 }
 
