97042b5a41fc23dd4afaedda1d9a18f8433778ce
Peter Park <peterxpark@gmail.com>
2014-10-31 22:23:59 -0400

    added scraper for BrainyQuote to CSV and updated index.php

--- index.php
+++ index.php
@@ -7,9 +7,17 @@
   <body>
     <?php
     $quotes = file("QuotesFromForbes.txt");
-    $randNum = rand(0, count($quotes));
+    $quotes2 = file("QuotesFromBrainyQuote.txt");
+    $quotes = array_merge($quotes, $quotes2);
+
+    $randNum = rand(0, count($quotes)-1);
     $pieces = explode(";", $quotes[$randNum]);
-    print $pieces[0];
+    $quote = $pieces[0];
+    $author = $pieces[1];
+    if(is_string($quote) AND is_string($author))
+       print $pieces[0] . " - " . $pieces[1];
+     else
+      print "Error";
     //print utf8_encode($pieces[0]);
 
     ?>
---
+++ scrape-BrainyQuoteToCSV.php
@@ -0,0 +1,48 @@
+<?php
+libxml_use_internal_errors(true);
+print "Program Starting...<br />";
+$ouFilename = "QuotesFromBrainyQuote.txt";
+$outfile = fopen($ouFilename, "wb");
+
+
+$url = "http://www.brainyquote.com/quotes/topics/topic_inspirational.html?vm=l";
+
+for($i=1;$i<12;$i++) {
+	if($i>1)
+		$url = "http://www.brainyquote.com/quotes/topics/topic_inspirational" . $i . ".html?vm=l";
+				
+	$dom = new DOMDocument;
+	$dom->loadHTMLFile($url);
+	$dom_xpath = new DOMXpath($dom);
+
+	$elements = $dom_xpath->query("//div[@class='boxyPaddingBig']");
+	if (!is_null($elements)) {
+	  foreach ($elements as $element) {
+
+		$node = $dom_xpath->query("span[@class='bqQuoteLink']", $element);
+		$quote = $node->item(0)->nodeValue;
+		//print $node->item(0)->nodeValue . "<br />";
+		$node = $dom_xpath->query("div[@class='bq-aut']", $element);
+		$author = $node->item(0)->nodeValue;
+		//print $node->item(0)->nodeValue . "<br />";
+
+
+        $quote = utf8_encode(trim($quote));
+        $author = utf8_encode(trim($author));
+        fwrite($outfile, $quote . ";" . $author . "\r\n");
+	  }
+	}
+
+
+}
+
+
+
+
+
+
+fclose($outfile);
+print 'Program Finished, go see your <a href="'. $ouFilename . '">new file</a>';
+
+
+?>
